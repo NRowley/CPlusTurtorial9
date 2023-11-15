b4c2ebb45635deb59e010fc7136ca474bddf70f0
NRowley <nicholas.ryan.rowley@gmail.com>
2023-11-15 14:38:12 -0600

    Classes

---
+++ Turtorial9/Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class dog {
+	string name;
+	int age;
+	float health;
+
+	void bark() {
+		cout << "woof \n";
+	}
+
+};
+
+int main() {
+	dog Halen;
+	dog Bowie;
+
+
+	system("pause");
+}
